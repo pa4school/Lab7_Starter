339784b85a74f974bf32947efd1ff4057c66feab
Anh Pham <phuanh004@gmail.com>
2023-05-20 23:49:03 -0700

    Update cache

--- sw.js
+++ sw.js
@@ -9,7 +9,14 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      return cache.addAll([
+        'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
+        'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
+        'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
+        'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
+      ]);
     })
   );
 });
@@ -39,17 +46,67 @@ self.addEventListener('fetch', async function (event) {
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
   // Is this request already in the cache?
-  if (event.request.url.includes('recipes.json')) {
-    // Open the cache
+  // Check if this request is already in the cache
+  if (event.request.destination === 'image') {
     event.respondWith(
-      // Respond with the cached version or from the network
-      caches.open(CACHE_NAME).then(async function (cache) {
-        const response = await fetch(event.request)
-        cache.put(event.request, response.clone())
-        return response
+      caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match(event.request).then(function (response) {
+          // If the request is in the cache
+          if (response) {
+            // Return the cached version
+            return response;
+          }
+          // If the request is NOT in the cache, fetch and cache
+          return fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone())
+            return response;
+          })
+        })
       })
-    );
-  } else {
-    return
+    )
   }
+
+  else {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match(event.request).then(function (response) {
+          // If the request is in the cache
+          if (response) {
+            // Return the cached version
+            return response;
+          }
+          // If the request is NOT in the cache, fetch and cache
+          return fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone())
+            return response;
+          })
+        })
+      })
+    )
+  }
+
+
+  // if (event.request.url.includes('recipes.json')) {
+  //   // Open the cache
+  //   event.respondWith(
+  //     // Respond with the cached version or from the network
+  //     caches.open(CACHE_NAME).then(async function (cache) {
+  //       const response = await fetch(event.request)
+  //       cache.put(event.request, response.clone())
+  //       return response
+  //     })
+  //   );
+  // } 
+  // else if (event.request.url.includes('image')) {
+  //   // Open the cache
+  //   event.respondWith(
+  //     // Response with cached first
+
+
+  //   );
+  // }
+
+  // else {
+  //   return 
+  // }
 });
