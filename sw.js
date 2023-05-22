e3627c66c04bf6e5897ecc58b3766a985db1bdba
Anh Pham <phuanh004@gmail.com>
2023-05-21 22:50:27 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,17 +28,6 @@ self.addEventListener('activate', function (event) {
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
 
@@ -54,7 +43,7 @@ self.addEventListener('fetch', function (event) {
           // If the request is in the cache
           if (response) {
             // Return the cached version
-            return response;
+            return response
           }
           // If the request is NOT in the cache, fetch and cache
           return fetch(event.request).then(function (response) {
@@ -73,12 +62,12 @@ self.addEventListener('fetch', function (event) {
           // If the request is in the cache
           if (response) {
             // Return the cached version
-            return response || fetch(event.request)
+            return response
           }
           // If the request is NOT in the cache, fetch and cache
           return fetch(event.request).then(function (response) {
             cache.put(event.request, response.clone())
-            return response || fetch(event.request)
+            return response
           })
         })
       })
