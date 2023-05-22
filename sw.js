9a2c49d01966254f40a666d09e75b08ffe760dca
Anh Pham <phuanh004@gmail.com>
2023-05-21 22:20:14 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@ self.addEventListener('install', function (event) {
         'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
         'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
         'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
-        `{DOMAINS}/assets/scripts/RecipeCard.js`,
+        `${DOMAINS}/assets/scripts/RecipeCard.js`,
       ])
     })
   )
